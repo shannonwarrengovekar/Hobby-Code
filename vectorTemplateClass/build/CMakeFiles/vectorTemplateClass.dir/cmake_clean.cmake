440b81ead304209c95e66eed616b9371385e112d
asambrekar11 <asambrek@andew.cmu.edu>
2018-04-11 23:19:26 -0400

    2

---
+++ vectorTemplateClass/build/CMakeFiles/vectorTemplateClass.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectorTemplateClass.dir/src/vectorTest.cpp.o"
+  "libvectorTemplateClass.pdb"
+  "libvectorTemplateClass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vectorTemplateClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ vectorTemplateClass/build/CMakeFiles/vectorTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectorTest.dir/src/main.cpp.o"
+  "vectorTest.pdb"
+  "vectorTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vectorTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
